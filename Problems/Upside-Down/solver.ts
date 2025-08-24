32d50cfa02a11afc06c29f0b9d0e47a04af1a90c
Dennis Bauer <d.bauer@cognigy.com>
2025-08-24 23:06:22 +0200

    Removed unnecessary if

--- Problems/Upside-Down/solver.ts
+++ Problems/Upside-Down/solver.ts
@@ -1,6 +1,4 @@
 export default function sameUpsideDown(string: string): boolean {
-
-  if (string.length <= 0) return true;
   if (string.split("").some(char => !["0","6","9"].includes(char))) return false;
 
   return string === string.split("").reverse().map(char => char === "6" ? "9" : char === "9" ? "6" : char).join("");
