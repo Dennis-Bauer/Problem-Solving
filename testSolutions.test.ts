998863fcdb3886f0660fedca2f99d887921b9ec8
Dennis Bauer <d.bauer@cognigy.com>
2025-08-24 21:15:10 +0200

    Added TypeScript and Vitest

---
+++ testSolutions.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import testProblem from "./Problems/TestProblem1"
+
+describe("IMPLEMENT-SOLUTION", () => {
+  it("should get implemented", () => {
+    expect(true).toBe(true);
+  })
+})
