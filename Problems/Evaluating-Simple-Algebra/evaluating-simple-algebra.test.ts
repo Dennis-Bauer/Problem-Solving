ad9da65fce773c6ee4b0a424d97dd54e88e537c5
Dennis <bauer.denni@icloud.com>
2025-11-17 18:22:36 +0100

    Solved the problem

--- Problems/Evaluating-Simple-Algebra/evaluating-simple-algebra.test.ts
+++ Problems/Evaluating-Simple-Algebra/evaluating-simple-algebra.test.ts
@@ -6,27 +6,27 @@ describe("Solves the equation correctly", () => {
 		expect(evaluatingSimpleAlgebra("2 + x = 19")).toEqual(17);
 	});
 
-		it("should return 3 for '4 - x = 1'", () => {
+	it("should return 3 for '4 - x = 1'", () => {
 		expect(evaluatingSimpleAlgebra("4 - x = 1")).toEqual(3);
 	});
 
-		it("should return 43 for 'x + 10 = 53'", () => {
+	it("should return 43 for 'x + 10 = 53'", () => {
 		expect(evaluatingSimpleAlgebra("x + 10 = 53")).toEqual(43);
 	});
 
-		it("should return 3 for '-23 + x = -20'", () => {
+	it("should return 3 for '-23 + x = -20'", () => {
 		expect(evaluatingSimpleAlgebra("-23 + x = -20")).toEqual(3);
 	});
 
-		it("should return -5 for '10 + x = 5'", () => {
+	it("should return -5 for '10 + x = 5'", () => {
 		expect(evaluatingSimpleAlgebra("10 + x = 5")).toEqual(-5);
 	});
 
-		it("should return 131 for '-49 - x = -180'", () => {
+	it("should return 131 for '-49 - x = -180'", () => {
 		expect(evaluatingSimpleAlgebra("-49 - x = -180")).toEqual(131);
 	});
 
-		it("should return -34 for 'x - 22 = -56'", () => {
+	it("should return -34 for 'x - 22 = -56'", () => {
 		expect(evaluatingSimpleAlgebra("x - 22 = -56")).toEqual(-34);
 	});
 });
--- Problems/Evaluating-Simple-Algebra/solver.ts
+++ Problems/Evaluating-Simple-Algebra/solver.ts
@@ -1,5 +1,31 @@
 export default function evaluatingSimpleAlgebra(task: string): number {
+	if (!/^[0-9+\-=x ]+$/.test(task))
+		throw Error("The given equation contains elements which are not allowed!");
 
-  return 0;
+	const [firstNumber, operator, secondNumber, , r] = task
+		.toLowerCase()
+		.split(" ");
+
+	if (!(operator === "+" || operator === "-"))
+		throw Error(
+			"The given equation does not contain an valid operator ('+' and '-')!",
+		);
+
+	const result = Number(r);
+
+	if (firstNumber === "x") {
+		const sNumber = Number(secondNumber);
+		if (operator === "+") return result - sNumber;
+		
+    return result + sNumber;
+	} else if (secondNumber === "x") {
+		const fNumber = Number(firstNumber);
+		if (operator === "+") return result - fNumber;
+	
+    return fNumber - result;
+	} else
+		throw Error(
+			"The given equation does not contain an x as the first or second number!",
+		);
 
 }
