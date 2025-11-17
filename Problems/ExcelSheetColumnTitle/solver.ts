746a58c7a3701e13b059a5b65f2c389ae1e2ec4a
Dennis <bauer.denni@icloud.com>
2025-11-17 17:20:35 +0100

    Solved the Problem

--- Problems/ExcelSheetColumnTitle/solver.ts
+++ Problems/ExcelSheetColumnTitle/solver.ts
@@ -1,27 +1,18 @@
 export default function convertToTitle(number: number): string {
 	const convertNumberToLetter = (n: number) => String.fromCharCode(n + 64);
 
-	// number = 25;
-
 	let x = number;
 	let result = "";
-	while (x > 0) {
-		result = convertNumberToLetter(Math.trunc(x % 26)) + result;
+	do {
+		const calculationResult = x % 26;
+		
+		if (calculationResult === 0) result = `Z${result}`;
+		else result = convertNumberToLetter(calculationResult) + result;
+		
+		if (x <= 26) break;
 
-		x = Math.trunc(x / 26);
-	}
-
-	// console.log(229704 / 26)
-	// console.log(convertNumberToLetter(229704 % 26))
-	// console.log(convertNumberToLetter(8834 % 26))
-
-	// console.log(convertNumberToLetter(Math.trunc(number / 26)) + convertNumberToLetter(Math.trunc(number % 26)))
+		x = Math.trunc((x - 1) / 26);
+	} while (x > 0);
 
 	return result;
 }
-
-// 26 - Z
-
-// 25 - Y
-
-// 27 - AZ
