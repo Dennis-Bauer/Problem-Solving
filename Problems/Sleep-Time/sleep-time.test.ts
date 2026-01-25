2ddeb0ff11d2bf9ac6a14b08f2036a7bb9363f6a
Dennis <bauer.denni@icloud.com>
2026-01-25 18:51:52 +0100

    Included error tests

--- Problems/Sleep-Time/sleep-time.test.ts
+++ Problems/Sleep-Time/sleep-time.test.ts
@@ -11,24 +11,28 @@ describe("Returns always the correct time to go to sleep", () => {
 	});
 
 	it("should return '22:00' for start time '08:00' and duration '10:00'", () => {
-		expect(bedTime("22:00", "08:00")).toEqual("10:00");
+		expect(bedTime("08:00", "10:00")).toEqual("22:00");
 	});
 
 	it("should return '23:30' for start time '09:30' and duration '10:00'", () => {
-		expect(bedTime("22:30", "09:30")).toEqual("10:00");
+		expect(bedTime("09:30", "10:00")).toEqual("23:30");
 	});
 
 	it("should return '01:45' for start time '05:45' and duration '04:00'", () => {
-		expect(bedTime("01:45", "05:45")).toEqual("04:00");
+		expect(bedTime("05:45", "04:00")).toEqual("01:45");
 	});
 
 	it("should return '02:40' for start time '07:10' and duration '04:30'", () => {
-		expect(bedTime("02:40", "07:10")).toEqual("04:30");
+		expect(bedTime("07:10", "04:30")).toEqual("02:40");
 	});
 });
 
-// describe("Another description for the tests. Maybe those which are throwing errors", () => {
-// 	it("should return something for this input", () => {
-// 		expect(true).toEqual(true);
-// 	});
-// });
+describe("Throws the correct error when having the wrong time format", () => {
+	it("should throw an Error because the hour is bigger than allowed", () => {
+		expect(() => bedTime("26:23", "00:00")).toThrowError("The given times haven't the correct format!");
+	});
+
+	it("should throw an Error because the minute is bigger than allowed", () => {
+		expect(() => bedTime("00:59", "00:80")).toThrowError("The given times haven't the correct format!");
+	});
+});
