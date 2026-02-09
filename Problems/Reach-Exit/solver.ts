18c9f4e7443dd03b4037285a7d9532632a88858c
Dennis <bauer.denni@icloud.com>
2026-02-09 15:47:56 +0100

    Start working on solution

--- Problems/Reach-Exit/solver.ts
+++ Problems/Reach-Exit/solver.ts
@@ -1,3 +1,10 @@
-export default function canReachExit(_maze: string[]): boolean {
-	throw new Error("Solution yet not implemented!");
+const symbole = [".", "#", "@", "E"];
+
+export default function canReachExit(givenMaze: string[]): boolean {
+	const maze: string[][] = givenMaze.map((val) => val.split(""));
+
+	// Input checking
+	if (maze.reduce((pre, cur) => (!pre ? false : cur.length !== maze[0].length), true)) return false;
+
+	return false;
 }
