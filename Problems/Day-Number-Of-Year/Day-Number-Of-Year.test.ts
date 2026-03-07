6e3dc50a9d965d01a92f622cb7e84647ae7412c1
Dennis Bauer <dennis.bauer@nice.com>
2026-03-07 21:49:12 +0100

    Setup problem

--- Problems/Day-Number-Of-Year/Day-Number-Of-Year.test.ts
+++ Problems/Day-Number-Of-Year/Day-Number-Of-Year.test.ts
@@ -1,13 +1,28 @@
 import { describe, expect, it } from "vitest";
+import dayOfYear from "./solver";
 
-describe("Description for the tests", () => {
-	it("should return something for this input", () => {
-		expect(true).toEqual(true);
+describe("Returns the correct day number of the year", () => {
+	it("should return 348 for 12/13/2020", () => {
+		expect(dayOfYear("12/13/2020")).toEqual(348);
 	});
-});
 
-describe("Another description for the tests. Maybe those which are throwing errors", () => {
-	it("should return something for this input", () => {
-		expect(true).toEqual(true);
+	it("should return 321 for 11/16/2020", () => {
+		expect(dayOfYear("11/16/2020")).toEqual(321);
+	});
+
+	it("should return 9 for 1/9/2019", () => {
+		expect(dayOfYear("1/9/2019")).toEqual(9);
+	});
+
+	it("should return 61 for 3/1/2004", () => {
+		expect(dayOfYear("3/1/2004")).toEqual(61);
+	});
+
+	it("should return 366 (leap) for 12/31/2000", () => {
+		expect(dayOfYear("12/31/2000")).toEqual(366);
+	});
+
+	it("should return 365 for 12/31/2019", () => {
+		expect(dayOfYear("12/31/2019")).toEqual(365);
 	});
 });
---
+++ Problems/Day-Number-Of-Year/solver.ts
@@ -0,0 +1,3 @@
+export default function dayOfYear(date: string): number {
+	throw new Error("Not implemented!");
+}
