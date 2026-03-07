3cd13a297b7e03d7310f2b9b1b08bda9788a4b22
Dennis Bauer <dennis.bauer@nice.com>
2026-03-07 21:39:47 +0100

    MAIN_COMMIT Fixed test file naming

---
+++ Problems/Day-Number-Of-Year/Day-Number-Of-Year.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+describe("Description for the tests", () => {
+	it("should return something for this input", () => {
+		expect(true).toEqual(true);
+	});
+});
+
+describe("Another description for the tests. Maybe those which are throwing errors", () => {
+	it("should return something for this input", () => {
+		expect(true).toEqual(true);
+	});
+});
