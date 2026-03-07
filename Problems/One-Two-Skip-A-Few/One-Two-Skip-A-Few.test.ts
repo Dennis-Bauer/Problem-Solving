9ca6c4e96907c700b2a997c4004a6121b2635285
Dennis Bauer <dennis.bauer@nice.com>
2026-03-07 18:46:41 +0100

    Solved the problem

--- Problems/One-Two-Skip-A-Few/One-Two-Skip-A-Few.test.ts
+++ Problems/One-Two-Skip-A-Few/One-Two-Skip-A-Few.test.ts
@@ -3,7 +3,7 @@ import howManyMissing from "./solver";
 
 describe("Returns the correct number of missing numbers", () => {
 	it("should return 4 for [1, 2, 3, 8, 9]", () => {
-		expect(howManyMissing([1, 2, 3, 5, 8])).toEqual(4);
+		expect(howManyMissing([1, 2, 3, 8, 9])).toEqual(4);
 	});
 
 	it("should return 1 for [1, 3]", () => {
@@ -15,14 +15,18 @@ describe("Returns the correct number of missing numbers", () => {
 	});
 
 	it("should return 5 for [1, 3, 5, 7, 9, 11]", () => {
-		expect(howManyMissing([3, 5, 7, 9, 11])).toEqual(5);
+		expect(howManyMissing([1, 3, 5, 7, 9, 11])).toEqual(5);
 	});
 
 	it("should return 0 for [5, 6, 7, 8]", () => {
 		expect(howManyMissing([5, 6, 7, 8])).toEqual(0);
 	});
 
-	it("should return 100 for [5, 6, 86, 87, 90, 97, 107]", () => {
-		expect(howManyMissing([5, 6, 86, 87, 90, 97, 107])).toEqual(100);
+	it("should return 0 for []", () => {
+		expect(howManyMissing([])).toEqual(0);
+	});
+
+	it("should return 100 for [5, 6, 86, 87, 90, 97, 111]", () => {
+		expect(howManyMissing([5, 6, 86, 87, 90, 97, 111])).toEqual(100);
 	});
 });
--- Problems/One-Two-Skip-A-Few/solver.ts
+++ Problems/One-Two-Skip-A-Few/solver.ts
@@ -1,3 +1,4 @@
 export default function howManyMissing(list: number[]): number {
-	throw new Error("Not implemented yet!");
+	if (list.length <= 0) return 0;
+	return list.at(-1)! - list.at(0)! - list.length + 1;
 }
