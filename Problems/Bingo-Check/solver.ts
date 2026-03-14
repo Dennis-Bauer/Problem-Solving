0044e1f169d77ac8b41a9328522a2de27e92d8e5
Dennis Bauer <dennis.bauer@nice.com>
2026-03-14 13:14:30 +0100

    Solved Problem

--- Problems/Bingo-Check/solver.ts
+++ Problems/Bingo-Check/solver.ts
@@ -1,5 +1,22 @@
 import type { FixedArray } from "../../utilities/types/fixedArray";
 
 export default function bingoCheck(card: FixedArray<FixedArray<"x" | number, 5>, 5>): boolean {
-	throw new Error("Solution yet not implemented!");
+	if (
+		card.reduce(
+			(preVal, row) => (preVal ? true : row.reduce((p, element) => (p ? element === "x" : false), true)),
+			false
+		)
+	)
+		return true;
+
+	let leftDiagonal = true;
+	let rightDiagonal = true;
+	for (let i = 0; i < card.length; i++) {
+		if (card.reduce((preVal, val) => (preVal ? val[i] === "x" : false), true)) return true;
+
+		if (!(leftDiagonal && card[i][i] === "x")) leftDiagonal = false;
+		if (!(rightDiagonal && card[i][5 - i] === "x")) rightDiagonal = false;
+	}
+
+	return rightDiagonal || leftDiagonal;
 }
